ee931786970a68d0c4b8863edc2ca6785bc4488a
Zack Cuddy <zcuddy23@gmail.com>
2019-08-15 23:09:49 -0500

    Styles and settings'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true
+    browser: true
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'standard'
   ],
   // add your custom rules here
   rules: {
+    "space-before-function-paren": 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': 0,
+    'semi': ['error', 'never'],
+    'quote-props': ["error", "as-needed", { "keywords": true, "unnecessary": false }]
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -5,24 +5,32 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'GielowKrebs',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+      { hid: 'description', name: 'description', content: 'Remote Accounting. Anywhere.  GielowKrebs Accounting.  The Accounting Office' },
+      { hid: 'og:image', name: 'og:image', content: 'https://gielowkrebs.com/images/Logo.jpg' },
+      { hid: 'og:title', name: 'og:title', content: 'GielowKrebs' },
+      { hid: 'og:url', name: 'og:url', content: 'https://gielowkrebs.com' },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'GielowKrebs' },
+      { hid: 'og:description', name: 'og:description', content: 'Remote Accounting. Anywhere.  GielowKrebs Accounting.  The Accounting Office' },
+      { hid: 'twitter:image', name: 'twitter:image', content: 'https://gielowkrebs.com/images/Logo.jpg' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'GielowKrebs' },
+      { hid: 'twitter:site', name: 'twitter:site', content: 'https://gielowkrebs.com' },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'Remote Accounting. Anywhere.  GielowKrebs Accounting.  The Accounting Office' }
+
+    ],
+    script: [
+      { src: 'https://kit.fontawesome.com/1107c06a2b.js' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
   css: [
+    { src: '@/assets/styles/app.scss', lang: 'scss' }
   ],
   /*
   ** Plugins to load before mounting the App
