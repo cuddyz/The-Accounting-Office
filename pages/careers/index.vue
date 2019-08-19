796b6e18085adda6ba9d48b970ecb695d2042090
Zack Cuddy <zcuddy23@gmail.com>
2019-08-18 22:56:12 -0500

    Careers page

--- pages/careers/index.vue
+++ pages/careers/index.vue
@@ -37,15 +37,32 @@ export default {
 
 <style lang="scss" scoped>
   @import './assets/styles/colors';
+  @import './assets/styles/breaks';
+
   article {
     display: grid;
     background-color: darken(color(greyVLight), 10%);
-    margin: 5rem;
+    margin: 5rem 1rem 1rem 1rem;
     border-radius: 5px;
 
+    @media (min-width: breaks(tablet)) {
+      margin: 5rem 3rem;
+    }
+
+    @media (min-width: breaks(laptop)) {
+      margin: 5rem;
+    }
+
     > section {
+      padding: 1rem;
       margin: auto;
       text-align: center;
+      font-size: 0.9em;
+
+      @media (min-width: breaks(phablet)) {
+        font-size: 1em;
+        padding: 2rem;
+      }
 
       input {
         line-height: 2;
