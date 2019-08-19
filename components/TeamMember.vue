fff0e3d4bc2a88d6be7f69e708eac7a1b029d086
Zack Cuddy <zcuddy23@gmail.com>
2019-08-18 20:30:44 -0500

    Team

---
+++ components/TeamMember.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="flex column p-2">
+    <div class="flex-1 flex justify-center mb-1">
+      <div class="img-container" :style="{ 'background-image': `url('images/${member.picture}'`}" :aria-label="member.name" />
+    </div>
+    <div class="bio flex column text-center flex-1">
+      <h2 class="uppercase normal mb-50 color-grey">{{ member.name }}</h2>
+      <h3 class="color-greyDark normal pb-50">{{ member.title }}</h3>
+      <p class="color-grey pb-50 italic">"{{ member.slogan }} <a v-if="member.link" :href="member.link" target="_blank">{{ member.link }}.</a>"</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TeamMember',
+  props: ['member']
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './assets/styles/colors';
+  @import './assets/styles/breaks';
+
+  section {
+    .bio {
+      align-items: center;
+      justify-content: flex-start;
+    }
+
+    h2 {
+      font-size: 1em;
+      border-bottom: 2px solid color(greyLight);
+      width: 80%;
+
+      @media (min-width: breaks(phone)) {
+       font-size: 1.2em;
+      }
+
+      @media (min-width: breaks(laptop)) {
+        font-size: 1.4em;
+      }
+    }
+
+    h3 {
+      font-size: 0.9em;
+
+      @media (min-width: breaks(phone)) {
+       font-size: 1.1em;
+      }
+
+      @media (min-width: breaks(laptop)) {
+        font-size: 1.2em;
+      }
+    }
+
+    p {
+      font-size: 0.8em;
+
+      @media (min-width: breaks(laptop)) {
+        font-size: 1em;
+      }
+    }
+
+    .img-container {
+      border-radius: 50%;
+      border: 5px solid color(grey);
+      height: 12rem;
+      width: 12rem;
+      background-position: top center;
+      background-size: cover;
+      background-repeat: no-repeat;
+
+      @media (min-width: breaks(phone)) {
+        height: 15rem;
+        width: 15rem;
+      }
+    }
+  }
+</style>
---
+++ pages/team/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <article>
+    <section class="banner flex flex-center">
+      <div class="overlay" />
+      <div class="text-center color-white relative">
+        <p>Team</p>
+        <h2>Collaboration. Trust. Teamwork.</h2>
+      </div>
+    </section>
+    <section class="team">
+      <team-member v-for="member in team" :key="member.name" :member="member" />
+    </section>
+  </article>
+</template>
+
+<script>
+import TeamMember from '@/components/TeamMember'
+export default {
+  name: 'TeamPage',
+  components: {
+    TeamMember
+  },
+  data() {
+    return {
+      team: [
+        {
+          name: 'Karch Gielow',
+          title: 'CEO and Co-Founder',
+          slogan: 'I will be there for you any hour of any day.  I will put you ahead of my whole family, and that means alot because I have 6 kids.',
+          picture: 'Karch HS.jpg'
+        },
+        {
+          name: 'Lydia Krebs',
+          title: 'Head of External Affairs and Co-Founder',
+          slogan: 'Take a look around, and yea, thats my name on the building.',
+          picture: 'Lydia HS.jpg'
+        },
+        {
+          name: 'Devon Knupp',
+          title: 'Head of Human Resources and Creative Director',
+          slogan: 'Put your complaint in the pile, I\'ll get to it eventually.  Actually I probably won\'t',
+          picture: 'Devon HS.jpg'
+        },
+        {
+          name: 'Charlie Hill',
+          title: 'Theoretical Analyst and Social Media Lead',
+          slogan: 'I here by swear to provide you with at least one hot and sweaty tweet a day.',
+          picture: 'Charles HS.jpg'
+        },
+        {
+          name: 'Matt Nienhaus',
+          title: 'Lead Administrative Assistant',
+          slogan: 'I live and breathe to serve.',
+          picture: 'Matt HS.jpg'
+        },
+        {
+          name: 'Troy',
+          title: 'Interim Document Co-Signer',
+          slogan: 'Hey I\'m Troy.',
+          picture: 'Troy HS.jpg'
+        },
+        {
+          name: 'Zack Cuddy',
+          title: 'Lead Web Developer and Technical Consult',
+          slogan: 'I make awesome websites like this one.  Check me out:',
+          link: 'https://zack-cuddy.com',
+          picture: 'Zack HS.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './assets/styles/colors';
+  .banner {
+    height: 60vh;
+    max-height: 60vh;
+    padding: 2rem 0 0 2rem;
+    margin-top: 10vh;
+
+    background: url('/images/Collab.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top center;
+    position: relative;
+
+    color: color(white);
+
+    .overlay {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      height: 100%;
+      opacity: 0.5;
+      background-color: color(black);
+    }
+
+    p {
+      font-size: 1.8em;
+    }
+    h2 {
+      font-size: 3em;
+    }
+  }
+
+  .team {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: 1fr 1fr;
+    padding: 2rem;
+  }
+</style>
